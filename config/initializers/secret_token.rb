6357db245fa1a2a3309234141db8bbf04ce05ac9
lindsaynauman <lindsay@smashingboxes.com>
2013-12-06 12:15:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CourseTracker::Application.config.secret_key_base = '355f5914bf1f75b95200c1b45f83254a4f31c00c2d44ece0a8f3de6ee8e3f4e99b868a0b6fdb1de66598a4e8477ec3b0dd2456bafe9994ecce07aedd205dacba'
