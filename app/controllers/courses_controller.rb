e8f919d4b047a578b29d2b9a59aec8b76918682e
lindsaynauman <lindsay@smashingboxes.com>
2013-12-16 09:13:47 -0500

    teacher can view students, not grades

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,6 +3,16 @@ class CoursesController < ApplicationController
 	def new
 		@course = Course.new
 	end
+
+  def show
+    @user = current_user
+    @course = Course.find_by(teacher_id: @user.id)
+    @students = []
+    @all_enroll = Enrollment.where(course_id: @course.id)
+    @all_enroll.each do |enrollment|
+      @students << User.find_by(id: enrollment.student_id)
+    end
+  end
 	
   def index
   	@user = current_user
