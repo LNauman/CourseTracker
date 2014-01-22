1c2e3d1f04d25fcee1628e14617ecb4a5b1e35af
lindsaynauman <lindsay@smashingboxes.com>
2014-01-22 14:36:18 -0500

    switch from carrierwave to dragonfly, fixed spacing on courses#show

--- app/models/refrence.rb
+++ app/models/refrence.rb
@@ -1,11 +1,11 @@
 class Refrence < ActiveRecord::Base
+  dragonfly_accessor :image
 
   belongs_to  :course
 
-  validates_presence_of :doc
   validates_presence_of :course_id
   validates_presence_of :name
 
-  mount_uploader :doc, DocUploader
+  # mount_uploader :doc, DocUploader
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121185219) do
+ActiveRecord::Schema.define(version: 20140122151910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 20140121185219) do
   end
 
   create_table "refrences", force: true do |t|
-    t.string   "doc",        null: false
     t.integer  "course_id",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name",       null: false
+    t.string   "image_uid",  null: false
   end
 
   create_table "semesters", force: true do |t|
