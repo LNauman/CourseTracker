7cdf8243a582b22ceecf404c3bf08eeac0fed76f
lindsaynauman <lindsay@smashingboxes.com>
2014-01-24 13:30:21 -0500

    switched css to sass

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -7,17 +7,6 @@ Dragonfly.app.configure do
   protect_from_dos_attacks true
   secret "9b60f46b91bc5b86fd64c7a2f360c0e0ae998bb202ad8c2effb7e9766357b925"
 
-
-def thumb(size='40x40#')
-  original = case doc.ext
-  when 'png', 'gif' ,'jpeg', 'jpg'
-    doc
-  when 'pdf'
-    Dragonfly[:images].fetch_file("/images/file_icons/pdf.png")
-  end
-  original.thumb(size).url
-end
-
   url_format "/media/:job/:name"
 
   datastore :file,
